cbdf979f6fccb04ba2a2f801a37cd3fd02712793
ebot <contact@edbotz.us>
2010-03-07 23:35:30 -0500

    Added custom iPhone layout.

--- umtl.rb
+++ umtl.rb
@@ -1,6 +1,11 @@
 #!/usr/bin/env ruby
 require 'sinatra'
 
+# iphone index
+get '/', :agent => /Mobile|webOS/ do
+  haml :iphone, :layout => false
+end
+
 # index
 get '/' do
   haml :index
@@ -23,4 +28,9 @@ end
 get '/stylesheets/application.css' do
   header 'Content-Type' => 'text/css; charset=utf-8'
   sass :application
+end
+
+get '/stylesheets/iphone.css' do
+  header 'Content-Type' => 'text/css; charset=utf-8'
+  sass :iphone
 end
