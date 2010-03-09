f2f231025762782136034b4f4ec64fcfda255d3e
ebot <contact@edbotz.us>
2010-03-08 19:57:24 -0500

    Simplified search workflow.

--- umtl.rb
+++ umtl.rb
@@ -11,14 +11,6 @@ get '/' do
   haml :index
 end
 
-# search
-get '/search' do
-  search = params['search'].gsub(' ', '%20')
-  redirect 'http://spica.mclinc.org/polaris/search/searchresults.aspx' <<
-           '?ctx=15.1033.0.0.2&term=' <<
-           search
-end
-
 # SASS stylesheets
 get '/stylesheets/grid.css' do
   header 'Content-Type' => 'text/css; charset=utf-8'
