c6eef69d2e1bc967f5a39eb9bb3740a2cd6f89ad
Ed Botzum <contact@edbotz.us>
2010-03-30 09:30:08 -0400

    Added new haml/sass conventions and changed the header function to content type for Sinatra 1.0.

--- umtl.rb
+++ umtl.rb
@@ -1,5 +1,7 @@
 #!/usr/bin/env ruby
 require 'sinatra'
+require 'haml'
+require 'sass'
 
 # iphone index
 get '/', :agent => /Mobile|webOS/ do
@@ -13,16 +15,16 @@ end
 
 # SASS stylesheets
 get '/stylesheets/grid.css' do
-  header 'Content-Type' => 'text/css; charset=utf-8'
+  content_type 'text/css', :charset => 'utf-8'
   sass :grid
 end
 
 get '/stylesheets/application.css' do
-  header 'Content-Type' => 'text/css; charset=utf-8'
+  content_type 'text/css', :charset => 'utf-8'
   sass :application
 end
 
 get '/stylesheets/iphone.css' do
-  header 'Content-Type' => 'text/css; charset=utf-8'
+  content_type 'text/css', :charset => 'utf-8'
   sass :iphone
 end
